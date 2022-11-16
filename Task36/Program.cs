c52cf51db9c30114c0bad8b47d26ded7cbb9a940
Katy <bev08021982@gmail.com>
2022-11-16 14:04:29 +0400

    Решила задачу 36

---
+++ Task36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях. //нечётные позиции массива, а не числа..
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+//МЕТОДЫ
+//метод создает массив из случайных чисел.
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+//метод выводит на печать массив.
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("] ");
+}
+
+//метод счиатет сумму нечётных элементов
+int SumOddElements (int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i <= arr.Length-1; i=i+2)
+    {
+    sum=sum+arr[i]; 
+    }
+    return sum;
+}
+
+//ВЫВОД РЕЗУЛЬТАТА
+int[] array = CreateArrayRndInt(4, -99, 99);
+PrintArray(array);
+Console.WriteLine($" -> cумма нечётных элементов {SumOddElements(array)}");
---
+++ Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементами массива.
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+//веществтвенные числа(дробьные). через псевдогенератор можно. создать три метода:
+//создать метод который находит максимальное значение
+//создать метод который находит минимальные значения
+//создать метод который находит разницу между максимальным и минимальным значениями.
+//можно вызывать метод из метода.
+
+double[] CreateArrayRnddouble(int size, int min, int max)
+{
+    double[] array = new double [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min; // создаем число массива в заданном диапазоне
+        array[i] = Math.Round(num, 1); // записываем в массив число округленное до 1 знака после запятой.
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double[] arr = CreateArrayRnddouble(5, 1, 99);
+PrintArray(arr);
